8cfd4b39689248fdea908963dab3d48c19128b02
Ion Toporivschi <ion.toporivschi@barsanti.edu.it>
2019-01-25 09:04:48 +0100

    25.01

--- File_Di_Progetto/File_Di_Progetto.ino
+++ File_Di_Progetto/File_Di_Progetto.ino
@@ -2,7 +2,10 @@ void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
 int puntiTotali = 0;
+int puntiMinimo = 30;
+int puntiMassimo = 100;
 int puntiAppoggio = 0;
+int appoggio = 0;
 int puntiMeta = 0;
 int avversario = 0;
 int programma = 0;
@@ -13,7 +16,11 @@ randomSeed(analogRead(0));
 
 void loop() {
   // put your main code here, to run repeatedly:
-controllo = false;
+while(puntiAppoggio == 0)
+{
+  ControllaPuntiMeta();
+}
+controllo = 0;
 programma = 0;
 avversario = 0;
 CCV("Programma 101");
@@ -27,13 +34,13 @@ if(avversario == 0){
 }
 
 void Controllo(int numero){
-  if(avversario == 1 && numero != 6){
+  if(avversario == 1 && numero != 6 && numero != 1){
     controllo = 1;
   }
-  if(avversario == 2 && numero != 5){
+  if(avversario == 2 && numero != 5 && numero != 2){
     controllo = 1;
   }
-  if(avversario == 3 && numero != 4){
+  if(avversario == 3 && numero != 4 && numero != 3){
     controllo = 1;
   } 
 }
@@ -45,6 +52,20 @@ void MacchinaSceglie(){
    }
 }
 
+void AggiungiPunti(int punti){
+  puntiTotali = puntiTotali + punti;
+}
+void ControllaPuntiMeta(){
+
+if(puntiMeta >= 30 && puntiMeta <= 100){
+  puntiAppoggio = 1;
+}
+else
+{
+
+}
+}
+
 void CCV(String nome){ //ControlloCondizioniVincita
   if(puntiTotali == puntiMeta){
     Serial.println(nome + " ha vinto");
@@ -52,5 +73,15 @@ void CCV(String nome){ //ControlloCondizioniVincita
   if(puntiTotali > puntiMeta){
     Serial.println(nome + " ha perso");
   }
+}
+
+void ControllaInt(){
+while(Serial.available() > 0){
+  int inChar = Serial.read();
+  if(isDigit(inChar)){
+    inString += (char)inChar;
+  }
+}
+appoggio = (int)inString;
 }
 
